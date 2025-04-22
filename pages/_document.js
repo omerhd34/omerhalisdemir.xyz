4f4e9946f4b7c5134b6b8606e4ad03ee041915eb
omerhalisd1 <demirome123@gmail.com>
2025-04-22 23:55:07 +0300

    Değişikliklerin açıklaması

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon.png" type="image/png" />
+      </Head>{" "}
       <body className="antialiased">
         <Main />
         <NextScript />
