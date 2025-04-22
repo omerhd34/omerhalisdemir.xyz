1f87d453c208d8b240a8691acd46572e4ac804e6
omerhalisd1 <demirome123@gmail.com>
2025-04-23 00:09:11 +0300

    Değişikliklerin açıklaması

--- components/About.jsx
+++ components/About.jsx
@@ -100,29 +100,29 @@ export default function About() {
        <div className="text-gray-300 text-sm">{content[language].email}</div>
       </div>
 
-      <div className="bg-gray-700 p-4 sm:p-6 rounded-lg shadow-md flex flex-col items-center justify-center hover:bg-gray-600 transition duration-300 group relative">
-       <div className="flex items-center justify-center gap-3 sm:gap-5 mt-1">
+      <div className="bg-gray-700 p-2 sm:p-6 rounded-lg shadow-md flex flex-col items-center justify-center hover:bg-gray-600 transition duration-300 group relative">
+       <div className="flex flex-wrap items-center justify-center gap-2 sm:gap-3 md:gap-4">
         <Link
          href="https://www.linkedin.com/in/%C3%B6mer-halis-demir-7a9b79169/"
          target="_blank"
          rel="noopener noreferrer"
-         className="w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center rounded-full border-2 border-[#0077b5] text-[#0077b5] hover:text-white hover:bg-[#0077b5] transition duration-500"
+         className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 flex items-center justify-center rounded-full border-2 border-[#0077b5] text-[#0077b5] hover:text-white hover:bg-[#0077b5] transition duration-500"
         >
-         <FaLinkedin size={20} />
+         <FaLinkedin className="text-sm sm:text-base md:text-lg" />
         </Link>
         <Link
          href="https://github.com/omerhd34"
          target="_blank"
          rel="noopener noreferrer"
-         className="w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center rounded-full border-2 border-[#ff0000] text-[#ff0000] hover:text-white hover:bg-[#ff0000] transition duration-500"
+         className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 flex items-center justify-center rounded-full border-2 border-[#ff0000] text-[#ff0000] hover:text-white hover:bg-[#ff0000] transition duration-500"
         >
-         <FaGithub size={20} />
+         <FaGithub className="text-sm sm:text-base md:text-lg" />
         </Link>
         <Link
          href={content[language].cvPath}
          target="_blank"
          rel="noopener noreferrer"
-         className="w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center rounded-full border-2 border-gray-400 text-gray-300 hover:text-white hover:bg-black transition duration-500 font-bold text-sm"
+         className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 flex items-center justify-center rounded-full border-2 border-gray-400 text-gray-300 hover:text-white hover:bg-black transition duration-500 font-bold text-xs sm:text-sm max-[300px]:mt-1 "
         >
          CV
         </Link>
